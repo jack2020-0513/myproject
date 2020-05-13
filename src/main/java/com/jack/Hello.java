87cc38b2f0c3ed936fddbc2f4aaeb046de2b0823
Jack Kao <jack_kao7@msn.com>
2020-05-13 16:14:04 +0800

    Initial commit

---
+++ src/main/java/com/jack/Hello.java
@@ -0,0 +1,9 @@
+package com.jack;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+        Person p1 = new Person();
+        p1.hello();
+    }
+}
---
+++ src/main/kotlin/com/jack/Hello.kt
@@ -0,0 +1,13 @@
+package com.jack
+
+fun main() {
+//    println("Hello Kotlin");
+//    People().hello();
+    val p = People();
+    p.hello();
+}
+class People {
+    fun hello(){
+        println("Hello Konlin");
+    }
+}
