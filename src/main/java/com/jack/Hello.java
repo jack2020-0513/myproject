e7fcaff3fa286641f59fd2d6803a2e50e3bfb973
Jack Kao <jack_kao7@msn.com>
2020-05-13 17:00:52 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/jack/Hello.java
+++ src/main/java/com/jack/Hello.java
@@ -5,5 +5,12 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p1 = new Person();
         p1.hello();
+        int age = 19;
+        Integer age2 = 19;
+        age2.byteValue();
+        char c = '我';
+        float weight = 66.5f;
+        boolean adult = true;
+        String name = "jack";
     }
 }
